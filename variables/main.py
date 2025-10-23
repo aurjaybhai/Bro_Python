ffaaae95a3d01ea2012948bc31a907f46f460387
aurjaybhai <jaypardule10@gmail.com>
2025-10-23 19:31:14 +0530

    finished the variables

---
+++ variables/main.py
@@ -0,0 +1,70 @@
+# Variable = A container for a value (string,integer,float,boolean)
+#               A variable behaves as if it was the value it contains
+print("Hello World......")
+print("It's really good")
+# ==> this is Pound sign
+
+#################################### Variables ####################################################
+
+# Integers
+
+first_name = "Bro"
+print(first_name)  ## You can use f-string along with variables
+food = "pizza"
+email = "Bro223@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your name is : {email}")
+
+# Integers
+
+age = 25
+# quantity = 3.5 # This could be "float" rather than integer
+quantity = 3
+num_of_students = 31
+print(f"You are {age} Years Old")
+print(f"You are buying {quantity} items")
+print(f"Your clase has {num_of_students} students")
+
+# Float
+price = 12.99
+gpa = 4.2
+distance = 5.6
+
+print(f"The price is ${price}")
+print(f"Your gpa is : {gpa}")
+print(f"You ran {distance}Km")
+
+# Boolean
+
+is_student = True
+# is_student = False
+
+print(f"Are you a student?: {is_student}")
+
+for_sale = True
+
+is_online = True
+
+# if is_online:
+#     print("You are online")
+# else:
+#     print("You are offline")
+
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are not a student")
+
+# if for_sale:
+#     print("This item is for sale")
+# else:
+#     print("That item is NOT for sale")
+
+
+# Assignment answer
+user_name = "rakesh"
+year = 2024
+bank_balance = 534.34
+ITR = True
