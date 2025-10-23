928cc3315ff02d2aafe1a58fd400752e66ba9c57
aurjaybhai <jaypardule10@gmail.com>
2025-10-23 09:35:51 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is my first python first program
+print("Hello World......")
+print("It's really good")
+
+# ==> this is Pound sign
+
+#################################### Variables ####################################################
+
+# Integers
+
+first_name = "Bro"
+print(first_name)  ## You can use f-string along with variables
+food = "pizza"
+email = "Bro223@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your name is : {email}")
+
+# Integers
+
+age = 25
+# quantity = 3.5 # This could be "float" rather than integer
+quantity = 3
+num_of_students = 31
+print(f"You are {age} Years Old")
+print(f"You are buying {quantity} items")
+print(f"Your clase has {num_of_students} students")
+
+# Float
